b41172d5ecc5b3194306df12c633c54579606c31
asm-saim <saim777m@gmail.com>
2026-06-21 08:43:26 +0600

    category btn layout implemented

--- js/index.js
+++ js/index.js
@@ -11,9 +11,12 @@ const loadCategory = (names) => {
     const categoriesBox = document.getElementById("categories");
     // {id: 8, category_name: 'Bamboo', small_description: 'Fast-growing versatile plants with woody stems.'}
     for (let name of names) {
-        console.log(name)
+        // console.log(name)
         const divForCategory = document.createElement("div")
-        divForCategory.innerHTML = `${name.category_name}`
+        divForCategory.innerHTML = `<div class="mx-3 my-2">
+        <button class="btn btn-sm w-full justify-start 
+        hover:bg-[#15803D] hover:text-white text-base font-medium">${name.category_name}</button>
+        </div>`
 
         categoriesBox.append(divForCategory)
     }

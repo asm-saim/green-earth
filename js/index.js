387610875a64713c2d8e6601659bb96194b3ca21
asm-saim <saim777m@gmail.com>
2026-06-21 16:09:35 +0600

    all trees cards creditential fetched and displayed

--- js/index.js
+++ js/index.js
@@ -30,5 +30,42 @@ const allTrees = () => {
     const url = "https://openapi.programming-hero.com/api/plants";
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => loadAllTrees(data.plants))
+};
+
+//load all trees
+const loadAllTrees = (trees) => {
+
+    const treesContainer = document.getElementById("all-trees");
+
+    // category
+    // :"Ornamental Plant"
+    // description:"A decorative palm with large, fan-shaped leaves. Suitable for both indoor decoration and garden landscapes."
+    // id:21
+    // image:"https://i.ibb.co.com/8gqbH5LC/fan-palm-min.jpg"
+    // name:"Fan Palm"
+    // price:900
+
+    for (let tree of trees) {
+        // console.log(tree);
+        const cardInfo=document.createElement("div")
+        cardInfo.innerHTML=`
+        <div class="bg-white p-4 rounded-lg">
+                        <img src="${tree.image}" alt="" class="w-full h-52 object-cover rounded-lg">
+                        <h1 class="font-semibold text-xl mt-2">${tree.name}</h1>
+                        <p class="font-normal text-sm mt-3">${tree.description}</p>
+                        <div class="flex justify-between items-center mt-2">
+                            <h2 class="text-[#15803D] bg-[#CFF0DC] px-2 font-semibold rounded-xl">${tree.category}</h2>
+                            <h2 class="font-semibold text-lg">${tree.price}</h2>
+                        </div>
+                        <button class="btn w-full rounded-full bg-[#15803D] text-white mt-3">
+                            Add to Cart</button>
+                    </div>
+        `
+        treesContainer.append(cardInfo);
+    }
+
 }
+
+allTrees();
+
